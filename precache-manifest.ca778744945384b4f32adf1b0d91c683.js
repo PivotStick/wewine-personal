3b0b2fe97bcb4c6a90e1dca15c11718c5e31fa24
Maxime <pivoorigami@gmail.com>
2020-01-28 23:15:51 +0100

    Updates

---
+++ precache-manifest.ca778744945384b4f32adf1b0d91c683.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d56b41d0996f16727cc59955ec4a80a",
+    "url": "/wewine-personal/index.html"
+  },
+  {
+    "revision": "01430f0a2bc4761bc60f",
+    "url": "/wewine-personal/static/css/main.233c2266.chunk.css"
+  },
+  {
+    "revision": "380b1e7f5a91987c72fb",
+    "url": "/wewine-personal/static/js/2.3f8c38cd.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/wewine-personal/static/js/2.3f8c38cd.chunk.js.LICENSE"
+  },
+  {
+    "revision": "01430f0a2bc4761bc60f",
+    "url": "/wewine-personal/static/js/main.893ed8de.chunk.js"
+  },
+  {
+    "revision": "9d917f26820b0a7aa4f1",
+    "url": "/wewine-personal/static/js/runtime-main.e4711481.js"
+  }
+]);
